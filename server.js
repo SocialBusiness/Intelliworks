98ba9f51e9aebe727d551207dccc642eb0d708a1
SocialBusiness <anjawielgoss@proton.me>
2025-05-15 01:52:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 //const client = new OpenAI();
 
 //const response = await client.responses.create({
-//    model: "gpt-4.1",
+//    model: "gpt-4.1-turbo",
 //    input: "Write a one-sentence bedtime story about a unicorn.",
 //});
 
